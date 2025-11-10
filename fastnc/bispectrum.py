added sine GLM; fixed sign division bug; reuse 3d bispec for ia

fastnc/bispectrum.py
@@ -755,11 +755,12 @@ class BispectrumBase:
             ells  = trigutils.ruv_to_x1x2x3(r,u,v)
             vs    = trigutils.x1x2x3_to_ruv(*ells, signed=True)[2]
             sign2 = np.sign(vs)
-            bk   *= sign1/sign2
+            bk   *= sign1 / np.where(sign2 == 0, 1, sign2)
 
         # multiply window 
         if hasattr(self, 'window_function'):
             bk *= self.window_function(ell1, ell2, ell3)
+
         return bk
 
     # multipole decomposition
@@ -1159,98 +1160,102 @@ class BispectrumTATT(BispectrumBase):
         if b3d is None:
             B_ddE, B_dEd, B_Edd, B_dEE, B_EEd, B_EdE, B_EEE, B_ddB, B_dBd, B_Bdd, B_dEB, B_dBE, B_EBd, B_BEd, B_BdE, B_EdB, B_EEB, B_EBE, B_BEE = self.ia_bispectrum(K1, K2, K3, Z, z_piv, A1, alphaIA, A2, alphaIA_2, bias_ta)
         else:
-            B_ddE, B_dEd, B_Edd, B_dEE, B_EEd, B_EdE, B_EEE, B_ddB, B_dBd, B_Bdd, B_dEB, B_dBE, B_EBd, B_BEd, B_BdE, B_EdB, B_EEB, B_EBE, B_BEE = b3d
+            if self.modes_used == 'E_and_B':
+                B_ddE, B_dEd, B_Edd, B_dEE, B_EEd, B_EdE, B_EEE, B_ddB, B_dBd, B_Bdd, B_dEB, B_dBE, B_EBd, B_BEd, B_BdE, B_EdB, B_EEB, B_EBE, B_BEE = b3d
+            elif self.modes_used == 'E':
+                B_ddE, B_dEd, B_Edd, B_dEE, B_EEd, B_EdE, B_EEE = b3d
+            elif self.modes_used == 'B':
+                B_ddB, B_dBd, B_Bdd, B_dEB, B_dBE, B_EBd, B_BEd, B_BdE, B_EdB, B_EEB, B_EBE, B_BEE = b3d
+            else:
+                raise ValueError(f"{self.modes_used=} is not supported.")
 
-        #print(scomb)
-        #if scomb[0] == 1 and scomb[1] == 1 and scomb[2] == 1:
-        #if scomb == 0.3:
-        #    print("kernel 1", kernel_1)
-        #    print("kernel 1", kernel_2)
-        #    print("kernel 1", kernel_3)
-        #    print("w1", W_1)
-        #    print("w2", W_2)
-        #    print("w3", W_3)
-        #    print("shape", np.shape(B_ddE))
-        #    print("B_ddE", B_ddE[:,11])
-            
+        def _integrate(integrand):
+            if integrand.shape[1] > 1:
+                bk = np.trapz(integrand, chi_los, axis=1)
+            else:
+                bk = integrand
+            return bk
+        
         adjust = 1 / chi_los * (1 + z_los) ** 3
-        integrand_ddE = kernel_1 * kernel_2 * W_3 * B_ddE * adjust
-        integrand_dEd = kernel_1 * W_2 * kernel_3 * B_dEd * adjust
-        integrand_Edd = W_1 * kernel_2 * kernel_3 * B_Edd * adjust
+        if self.modes_used == 'E' or self.modes_used == 'E_and_B':
+            bk_ddE = _integrate(kernel_1 * kernel_2 * W_3 * B_ddE * adjust)
+            bk_dEd = _integrate(kernel_1 * W_2 * kernel_3 * B_dEd * adjust)
+            bk_Edd = _integrate(W_1 * kernel_2 * kernel_3 * B_Edd * adjust)
+            
+            bk_dEE = _integrate(kernel_1 * W_2 * W_3 * B_dEE * adjust)
+            bk_EEd = _integrate(W_1 * W_2 * kernel_3 * B_EEd * adjust)
+            bk_EdE = _integrate(W_1 * kernel_2 * W_3 * B_EdE * adjust)
+            
+            bk_EEE = integrate(W_1 * W_2 * W_3 * B_EEE * adjust)
 
-        integrand_ddB = kernel_1 * kernel_2 * W_3 * B_ddB * adjust
-        integrand_dBd = kernel_1 * W_2 * kernel_3 * B_dBd * adjust
-        integrand_Bdd = W_1 * kernel_2 * kernel_3 * B_Bdd * adjust
-                
-        integrand_dEE = kernel_1 * W_2 * W_3 * B_dEE * adjust
-        integrand_EEd = W_1 * W_2 * kernel_3 * B_EEd * adjust
-        integrand_EdE = W_1 * kernel_2 * W_3 * B_EdE * adjust
-
-        integrand_dEB = kernel_1 * W_2 * W_3 * B_dEB * adjust
-        integrand_dBE = kernel_1 * W_2 * W_3 * B_dBE * adjust
-        integrand_EBd = W_1 * W_2 * kernel_3 * B_EBd * adjust
-        integrand_BEd = W_1 * W_2 * kernel_3 * B_BEd * adjust
-        integrand_EdB = W_1 * kernel_2 * W_3 * B_EdB * adjust
-        integrand_BdE = W_1 * kernel_2 * W_3 * B_BdE * adjust
-
-        integrand_EEE = W_1 * W_2 * W_3 * B_EEE * adjust
+        elif self.modes_used == 'B' or self.modes_used == 'E_and_B':
+            bk_ddB = _integrate(kernel_1 * kernel_2 * W_3 * B_ddB * adjust)
+            bk_dBd = _integrate(kernel_1 * W_2 * kernel_3 * B_dBd * adjust)
+            bk_Bdd = _integrate(W_1 * kernel_2 * kernel_3 * B_Bdd * adjust)
+            
+            bk_dEB = _integrate(kernel_1 * W_2 * W_3 * B_dEB * adjust)
+            bk_dBE = _integrate(kernel_1 * W_2 * W_3 * B_dBE * adjust)
+            bk_EBd = _integrate(W_1 * W_2 * kernel_3 * B_EBd * adjust)
+            bk_BEd = _integrate(W_1 * W_2 * kernel_3 * B_BEd * adjust)
+            bk_EdB = _integrate(W_1 * kernel_2 * W_3 * B_EdB * adjust)
+            bk_BdE = _integrate(W_1 * kernel_2 * W_3 * B_BdE * adjust)
+    
+            bk_EEB = _integrate(W_1 * W_2 * W_3 * B_EEB * adjust)
+            bk_EBE = _integrate(W_1 * W_2 * W_3 * B_EBE * adjust)
+            bk_BEE = _integrate(W_1 * W_2 * W_3 * B_BEE * adjust)
         
-        integrand_EEB = W_1 * W_2 * W_3 * B_EEB * adjust
-        integrand_EBE = W_1 * W_2 * W_3 * B_EBE * adjust
-        integrand_BEE = W_1 * W_2 * W_3 * B_BEE * adjust
-        
-        # Integrate each component
-        if integrand_ddE.shape[1] > 1:
-            bk_ddE = np.trapz(integrand_ddE, chi_los, axis=1)
-            bk_dEd = np.trapz(integrand_dEd, chi_los, axis=1)
-            bk_Edd = np.trapz(integrand_Edd, chi_los, axis=1)
+        # # Integrate each component
+        # if integrand_ddE.shape[1] > 1:
+        #     bk_ddE = np.trapz(integrand_ddE, chi_los, axis=1)
+        #     bk_dEd = np.trapz(integrand_dEd, chi_los, axis=1)
+        #     bk_Edd = np.trapz(integrand_Edd, chi_los, axis=1)
 
-            bk_ddB = np.trapz(integrand_ddB, chi_los, axis=1)
-            bk_dBd = np.trapz(integrand_dBd, chi_los, axis=1)
-            bk_Bdd = np.trapz(integrand_Bdd, chi_los, axis=1)
+        #     bk_ddB = np.trapz(integrand_ddB, chi_los, axis=1)
+        #     bk_dBd = np.trapz(integrand_dBd, chi_los, axis=1)
+        #     bk_Bdd = np.trapz(integrand_Bdd, chi_los, axis=1)
             
-            bk_dEE = np.trapz(integrand_dEE, chi_los, axis=1)
-            bk_EEd = np.trapz(integrand_EEd, chi_los, axis=1)
-            bk_EdE = np.trapz(integrand_EdE, chi_los, axis=1)
+        #     bk_dEE = np.trapz(integrand_dEE, chi_los, axis=1)
+        #     bk_EEd = np.trapz(integrand_EEd, chi_los, axis=1)
+        #     bk_EdE = np.trapz(integrand_EdE, chi_los, axis=1)
 
-            bk_dEB = np.trapz(integrand_dEB, chi_los, axis=1)
-            bk_dBE = np.trapz(integrand_dBE, chi_los, axis=1)
-            bk_EBd = np.trapz(integrand_EBd, chi_los, axis=1)
-            bk_BEd = np.trapz(integrand_BEd, chi_los, axis=1)
-            bk_BdE = np.trapz(integrand_BdE, chi_los, axis=1)
-            bk_EdB = np.trapz(integrand_EdB, chi_los, axis=1)
+        #     bk_dEB = np.trapz(integrand_dEB, chi_los, axis=1)
+        #     bk_dBE = np.trapz(integrand_dBE, chi_los, axis=1)
+        #     bk_EBd = np.trapz(integrand_EBd, chi_los, axis=1)
+        #     bk_BEd = np.trapz(integrand_BEd, chi_los, axis=1)
+        #     bk_BdE = np.trapz(integrand_BdE, chi_los, axis=1)
+        #     bk_EdB = np.trapz(integrand_EdB, chi_los, axis=1)
             
-            bk_EEE = np.trapz(integrand_EEE, chi_los, axis=1)
+        #     bk_EEE = np.trapz(integrand_EEE, chi_los, axis=1)
 
-            bk_EEB = np.trapz(integrand_EEB, chi_los, axis=1)
-            bk_EBE = np.trapz(integrand_EBE, chi_los, axis=1)
-            bk_BEE = np.trapz(integrand_BEE, chi_los, axis=1)
+        #     bk_EEB = np.trapz(integrand_EEB, chi_los, axis=1)
+        #     bk_EBE = np.trapz(integrand_EBE, chi_los, axis=1)
+        #     bk_BEE = np.trapz(integrand_BEE, chi_los, axis=1)
 
-        else:
-            bk_ddE = kernel_1 * kernel_2 * W_3 * B_ddE
-            bk_dEd = kernel_1 * W_2 * kernel_3 * B_dEd
-            bk_Edd = W_1 * kernel_2 * kernel_3 * B_Edd
+        # else:
+        #     bk_ddE = kernel_1 * kernel_2 * W_3 * B_ddE # Why we do not have adjust here?
+        #     bk_dEd = kernel_1 * W_2 * kernel_3 * B_dEd
+        #     bk_Edd = W_1 * kernel_2 * kernel_3 * B_Edd
 
-            bk_ddB = kernel_1 * kernel_2 * W_3 * B_ddB
-            bk_dBd = kernel_1 * W_2 * kernel_3 * B_dBd
-            bk_Bdd = W_1 * kernel_2 * kernel_3 * B_Bdd
+        #     bk_ddB = kernel_1 * kernel_2 * W_3 * B_ddB
+        #     bk_dBd = kernel_1 * W_2 * kernel_3 * B_dBd
+        #     bk_Bdd = W_1 * kernel_2 * kernel_3 * B_Bdd
             
-            bk_dEE = kernel_1 * W_2 * W_3 * B_dEE
-            bk_EEd = W_1 * W_2 * kernel_3 * B_EEd
-            bk_EdE = W_1 * kernel_2 * W_3 * B_EdE
+        #     bk_dEE = kernel_1 * W_2 * W_3 * B_dEE
+        #     bk_EEd = W_1 * W_2 * kernel_3 * B_EEd
+        #     bk_EdE = W_1 * kernel_2 * W_3 * B_EdE
 
-            bk_dEB = kernel_1 * W_2 * W_3 * B_dEB * adjust
-            bk_dBE = kernel_1 * W_2 * W_3 * B_dBE * adjust
-            bk_EBd = W_1 * W_2 * kernel_3 * B_EBd * adjust
-            bk_BEd = W_1 * W_2 * kernel_3 * B_BEd * adjust
-            bk_EdB = W_1 * kernel_2 * W_3 * B_EdB * adjust
-            bk_BdE = W_1 * kernel_2 * W_3 * B_BdE * adjust
+        #     bk_dEB = kernel_1 * W_2 * W_3 * B_dEB * adjust
+        #     bk_dBE = kernel_1 * W_2 * W_3 * B_dBE * adjust
+        #     bk_EBd = W_1 * W_2 * kernel_3 * B_EBd * adjust
+        #     bk_BEd = W_1 * W_2 * kernel_3 * B_BEd * adjust
+        #     bk_EdB = W_1 * kernel_2 * W_3 * B_EdB * adjust
+        #     bk_BdE = W_1 * kernel_2 * W_3 * B_BdE * adjust
 
-            bk_EEE = W_1 * W_2 * W_3 * B_EEE
+        #     bk_EEE = W_1 * W_2 * W_3 * B_EEE
 
-            bk_EEB = W_1 * W_2 * W_3 * B_EEB * adjust
-            bk_EBE = W_1 * W_2 * W_3 * B_EBE * adjust
-            bk_BEE = W_1 * W_2 * W_3 * B_BEE * adjust
+        #     bk_EEB = W_1 * W_2 * W_3 * B_EEB * adjust
+        #     bk_EBE = W_1 * W_2 * W_3 * B_EBE * adjust
+        #     bk_BEE = W_1 * W_2 * W_3 * B_BEE * adjust
 
         if select_mode == None:
 

fastnc/coupling.py
@@ -19,6 +19,8 @@ try:
     from mpi4py import MPI
 except:
     print("Cannot import mpi4py")
+    from .utils import DummyMPI
+    MPI = DummyMPI()
 
 def get_cache_dir():
     # first we look for the environmental variable
@@ -249,7 +251,7 @@ class ModeCouplingFunctionBase:
             out = out[:,0]
 
         return out
-        
+
 class MCF222LegendreFourier(ModeCouplingFunctionBase):
     r"""
     Mode coupling function for spin-2, spin-2, spin-2 correlation function
@@ -447,3 +449,112 @@ class MCF222FourierFourier(ModeCouplingFunctionBase):
 
     def correct(self):
         pass
+
+class MCF222SineFourier(ModeCouplingFunctionBase):
+    r"""
+    Mode coupling function for spin-2, spin-2, spin-2 correlation function
+    and decomposition using Sine function and Fourier modes
+    for Fourier-space and real-space respectively.
+
+    .. math:
+        G_LM(\\psi) = 4\\pi \\int_0^{\\pi} dx \\sin[Lx]\\sin[M x + 2\\beta(\\psi, x)]
+    """
+    __doc__ += ModeCouplingFunctionBase.__doc__
+
+    # name of the mode coupling function
+    name = 'MCF222SineFourier'
+    
+
+    def _integrand(self, x, psi, L, M):
+        x, psi = np.meshgrid(x, psi, indexing='ij')
+        sin2bb, cos2bb = sincos2angbar(psi, x)
+        out = 4*np.pi*np.sin(L*x) * (np.sin(M*x)*cos2bb + np.cos(M*x)*sin2bb)
+        return out
+        
+    def compute(self):
+        has_changed = False
+        # prepare todo list
+        todo = []
+        for L in range(self.Lmax+1):
+            for M in range(self.Mmax+1):
+                todo.append([L, M])
+        # compute GLM
+        for L, M in todo:
+            print(f'\r(L,M) = {(L,M)}/{len(todo)}', end='') if self.verbose else None
+            # skip if the data already exists
+            # in the cache
+            if (L,M) in self.data:
+                continue
+            args = {'L':L, 'M':M, 'psi':self.psi}
+            o, c = aint(self._integrand, 0, np.pi, 2, tol=self.tol, **args)
+            self.data[(L, M)] = o
+            has_changed = True
+        return has_changed
+
+    def correct(self):
+        """
+        Correct the mode coupling function for numerical bias.
+        
+        G_LM(psi) is exactly zero for L<M and psi<=pi/4. 
+        However, the numerical integration may give a non-zero value.
+        From my experience, the same amount of error is also present in
+        G_ML if G_LM is biased. Hence we subtract the error in G_LM(psi<=pi/4)
+        from G_LM(psi) and G_ML(psi).
+        """
+        for (L,M), data in self.data.items():
+            if L>=M:
+                continue
+            # estimate the bias in G_LM(psi<=np.pi/4)
+            bias = np.mean(data[self.psi<=np.pi/4])
+            # subtract the bias
+            self.data[(L,M)] -= bias
+            if (M,L) in self.data:
+                self.data[(M,L)] -= bias
+
+    def __call__(self, L, M, psi):
+        Lisscalar = np.isscalar(L)
+        if Lisscalar:
+            L = np.array([L])
+        Misscalar = np.isscalar(M)
+        if Misscalar:
+            M = np.array([M])
+
+        if np.any(L>self.Lmax):
+            raise ValueError('L={} is larger than Lmax={}'.format(L, self.Lmax))
+        if np.any(L<0):
+            raise ValueError('L={} is smaller than 0'.format(L))
+        if np.any(M>self.Mmax):
+            raise ValueError('M={} is larger than Mmax={}'.format(M, self.Mmax))
+        if np.any(M<-self.Mmax):
+            raise ValueError('M={} is smaller than -Mmax={}'.format(M, self.Mmax))
+
+        # collect todo
+        todo = []
+        for _L in L:
+            for _M in M:
+                todo.append([_L, _M])
+
+        # Avoid the same computation
+        shape = psi.shape
+        psi_unique, inv = np.unique(psi.ravel(), return_inverse=True)
+
+        out = []
+        for _L, _M in todo:
+            # Use the symmetry for M<0
+            # G_{LM}(psi) = -G_{L(-M)}(np.pi/2-psi)
+            if _M>0:
+                o = np.interp(psi_unique, self.psi, self.data[(_L, _M)])
+            else:
+                o = -np.interp(np.pi/2-psi_unique, self.psi, self.data[(_L, -_M)])
+            o = o[inv].reshape(shape)
+            out.append(o)
+        out = np.array(out).reshape(L.shape+M.shape+psi.shape)
+
+        if Lisscalar and Misscalar:
+            out = out[0,0]
+        elif Lisscalar:
+            out = out[0]
+        elif Misscalar:
+            out = out[:,0]
+
+        return out

fastnc/ia_bispectra.py
@@ -43,6 +43,19 @@ class BispectraIA:
         self.cosmo = cosmo
         self.has_changed = True
 
+    # def set_IA_param(self, ia_params):
+    #     """
+    #     dict of z_piv, A1, alphaIA, A2, alphaIA_2, bias_ta
+    #     """
+    #     assert "z_piv"     in ia_params
+    #     assert "A1"        in ia_params
+    #     assert "alphaIA"   in ia_params
+    #     assert "A2"        in ia_params
+    #     assert "alphaIA_2" in ia_params
+    #     assert "bias_ta"   in ia_params
+    #     self.ia_params = ia_params
+    #     self.has_changed = True
+
     def update(self):
         if self.has_changed:
             self._normalize_pklin()
@@ -249,6 +262,13 @@ class BispectraIA:
 
         return result
 
+    def get_delta_K(self, m_val):
+        # Kronecker delta definition: zero if m is not zero, and N0^(-1) if m is zero
+        if m_val == 0.0:
+            return np.sqrt(2/3)
+        else:
+            return 0.0
+    
     def get_B000(self, k1_mag, k2_mag, k3_mag, PL1, PL2, PL3):
         #This is not used, here for completeness
         F0_12 = self.get_F_00(k1_mag, k2_mag, k3_mag, PL1, PL2, 0, 0, 0, mode='sss')
@@ -278,13 +298,6 @@ class BispectraIA:
 
         else:
             raise ValueError("m_val must be 0, 1, or 2 for get_B002")
-
-    def get_delta_K(self, m_val):
-        # Kronecker delta definition: zero if m is not zero, and N0^(-1) if m is zero
-        if m_val == 0.0:
-            return np.sqrt(2/3)
-        else:
-            return 0.0
 
     def get_B022(self, k1_mag, k2_mag, k3_mag, PL1, PL2, PL3, cg1, cg2_2, cg2_3, m2_val, m3_val):
 
@@ -361,6 +374,40 @@ class BispectraIA:
         term3 = delta_K_0m3 * delta_K_0m1 * F_31_m2
         return term1 + term2 + term3
 
+    # def get_cg(self, z):
+    #     # Get params
+    #     z_piv     = self.ia_params['z_piv']
+    #     A1        = self.ia_params['A1']
+    #     alphaIA   = self.ia_params['alphaIA']
+    #     A2        = self.ia_params['A2']
+    #     alphaIA_2 = self.ia_params['alphaIA_2']
+    #     bias_ta   = self.ia_params['bias_ta']
+
+    #     #Get C1, C1delta and C2 params
+    #     c1rhocrit = 0.0134
+    #     C1 = -A1 * ((1+z)/(1+z_piv))**alphaIA * c1rhocrit * self.cosmo['Om0'] / self.z2lgr(z)
+    #     C1delta = bias_ta*C1
+    #     C2 = A2 * ((1+z)/(1+z_piv))**alphaIA_2 * 5 * c1rhocrit * self.cosmo['Om0'] / self.z2lgr(z)**2
+
+    #     # normalization to match the A1 of TATT, according to Eq. 66 of Bakx et al (1) (2025) and Eq. 30 of Bakx et. al. (2) (2025)
+    #     C1 *= 2
+
+    #     #Get cg1, cg2_2 and cg2_3 params
+    #     cg1 = C1
+    #     cg2_2 = C2
+    #     cg2_3 = 0.5 * (3 * C1delta - C2)
+
+    #     return cg1, cg2_2, cg2_3
+
+    def get_B_dEE(self, k1_mag, k2_mag, k3_mag, z, PL1=None, PL2=None, PL3=None, cg1=None, cg2_2=None, cg2_3=None):
+            
+        
+        # B_delta_delta_E (Eq. 12)
+        B_002_0 = self.get_B002(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3, cg1, cg2_2, cg2_3, 0)
+        B_002_2 = self.get_B002(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3, cg1, cg2_2, cg2_3, 2)
+        B_ddE = 0.5 * (np.sqrt(3/2) * B_002_0 - B_002_2)        
+
+    # Get terms of TATT IA bispectrum
     def get_ia_bispectra(self, k1_mag, k2_mag, k3_mag, z, z_piv, A1, alphaIA, A2, alphaIA_2, bias_ta, renormalize=True, do_non_linear=True):
 
         #Get C1, C1delta and C2 params
@@ -377,9 +424,6 @@ class BispectraIA:
         cg2_2 = C2
         cg2_3 = 0.5 * (3 * C1delta - C2)
 
-        # Construct k-vectors
-        #k1_vec, k2_vec, k3_vec = self._construct_k_vectors(k1_mag, k2_mag, k3_mag)
-
         # Get linear power spectra
         if do_non_linear:
             PL1 = self.get_interpolated_pknl(k1_mag, z)
@@ -389,7 +433,6 @@ class BispectraIA:
             PL1 = self.get_interpolated_pklin(k1_mag, z)
             PL2 = self.get_interpolated_pklin(k2_mag, z)
             PL3 = self.get_interpolated_pklin(k3_mag, z)
-
 
         # B_delta_delta_E (Eq. 12)
         B_002_0 = self.get_B002(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3, cg1, cg2_2, cg2_3, 0)
@@ -448,10 +491,6 @@ class BispectraIA:
         # B_delta_BE (permutation)
         B_022_01 = self.get_B022(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3, cg1, cg2_2, cg2_3, 1, 0)
         B_dBE = -np.sqrt(3/8) * B_022_01
-        # k3_vec, k1_vec, k2_vec = self._construct_k_vectors(k3_mag, k1_mag, k2_mag)
-        # # B_delta_BE (permutation)
-        # B_022_01 = self.get_B022(k3_vec, k1_vec, k2_vec, k3_mag, k1_mag, k2_mag, PL3, PL1, PL2, cg1, cg2_2, cg2_3, 0, 1)
-        # B_EBd = -np.sqrt(3/8) * B_022_01
 
         # B_B_delta_E (permutation)
         B_022_01 = self.get_B022(k2_mag, k3_mag, k1_mag, PL2, PL3, PL1, cg1, cg2_2, cg2_3, 0, 1)